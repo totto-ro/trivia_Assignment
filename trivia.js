b8df0d370415f2c6ea245135d8fb48a555f66480
Sofía Esquivel <sofia.esquivel@lcieducation.net>
2021-12-03 16:55:23 -0600

    Updated

--- trivia.js
+++ trivia.js
@@ -2,11 +2,23 @@ console.log("hello");
 
 
 function getQuery( event ){
-    $.get( "https://opentdb.com/api.php?amount=10&category=11", displayData )
+
+    new Promise((resolve, reject) => {
+        $.get({
+            url: `https://opentdb.com/api.php?amount=10&category=11`,
+            success: resolve,
+            error: reject,
+        });
+        })
+        .then(displayData)
+        .catch(error => {
+            
+            console.log(`Something went wrong...`);
+        });
+
     function displayData( data ) {
         console.log( data.results );
         let questions = document.querySelector('.triviaInfo')
-        
         for(let value in data.results){
             console.log( data.results[value].question);
             questions.innerHTML += `
@@ -14,10 +26,17 @@ function getQuery( event ){
                 <h2>
                     ${data.results[value].question}
                 </h2>
-                <div class="answer">
-                    <li>${data.results[value].correct_answer}</li>
-                </div>
+                
+                <li> <strong> Correct answer: </strong> ${data.results[value].correct_answer}</li>
             `;
+            
+        }
+        let answers = document.querySelectorAll('.answer')
+        for(let value in data.results){
+            answers.innerHTML += `
+                <li>${data.results[value].correct_answer}</li>
+            `;
+
         }
         
     }
@@ -25,8 +44,4 @@ function getQuery( event ){
 
 let querybutton = document.querySelector('.button')
 querybutton.addEventListener('click', getQuery);
-
-
-
-
 
